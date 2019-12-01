35a28c69115259701ce33fe55bdd8b4f10c839bc
ChengJiacheng <31717437+ChengJiacheng@users.noreply.github.com>
2019-12-01 13:32:27 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import os, torch, torchtext
+    
+from torchtext import data
+from torchtext import datasets
+from torchtext.vocab import GloVe
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+print("PyTorch Version: ", torch.__version__)
+print("torchtext Version: ", torchtext.__version__)
+
+from models import Naive_Clf, IMDBRnn, IMDBCnn
+    
+
+def fit(epoch,model,data_loader,  phase='training'):
+    if phase == 'training':
+        model.train()
+    if phase == 'validation':
+        model.eval()
+        
+    optimizer.zero_grad()
+    running_loss = 0.0
+    running_correct = 0
+    
+    for batch_idx , batch in enumerate(data_loader):
+        text, labels = batch.text , batch.label - 1
+        if use_gpu:
+            text, labels = text.cuda(), labels.cuda()
+        with torch.set_grad_enabled(phase == 'training'):        
+       
+            output = model(text)
+    #        print(output.shape)
+            loss = F.cross_entropy(output, labels)
+            
+            running_loss += loss.cpu().item()
+            preds = output.data.max(dim=1,keepdim=True)[1]
+            running_correct += preds.eq(labels.data.view_as(preds)).cpu().sum().item()
+            
+            if phase == 'training':
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+    
+    loss = running_loss/len(data_loader.dataset)
+    accuracy = 100. * running_correct/len(data_loader.dataset)
+    
+    print(phase, 'loss:', loss, 'acc:', accuracy)
+    return loss, accuracy
+
+if __name__ == '__main__':
+    import argparse
+    import pprint
+    _utils_pp = pprint.PrettyPrinter()
+    def pprint(x):
+        _utils_pp.pprint(x)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--fix_length', type=int, default=200)
+    parser.add_argument('--max_vocab_size', type=int, default=25000)
+    parser.add_argument('--dim_embdeding', type=int, default=300, choices=[100, 300])
+    parser.add_argument('--pretrained', default=True, choices=[True, False])
+    parser.add_argument('--model', type=str, default='LSTM', choices=['Naive', 'CNN', 'LSTM'])
+    parser.add_argument('--epochs', type=int, default=10)
+
+    args = parser.parse_args()
+    
+    pprint(args)
+
+    use_gpu = torch.cuda.is_available()
+    
+    TEXT = data.Field(lower=True, fix_length=args.fix_length, batch_first=True)
+    LABEL = data.Field(sequential=False,)
+    
+    
+    
+    train, test = datasets.IMDB.splits(TEXT, LABEL)
+    
+    
+    dim_embdeding = 300
+    
+    TEXT.build_vocab(train, vectors=GloVe(name='6B', dim=dim_embdeding), max_size=args.max_vocab_size, min_freq=10)
+    LABEL.build_vocab(train,)
+    
+    
+    train_iter, test_iter = data.BucketIterator.splits((train, test), batch_size= args.batch_size)
+    train_iter.repeat = False
+    test_iter.repeat = False
+       
+    
+#    batch = next(iter(train_iter))    
+    
+    num_classes = 2
+    
+    if args.model == 'Naive':    
+        model = Naive_Clf(vocab_size = TEXT.vocab.vectors.shape[0], dim_embdeding = TEXT.vocab.vectors.shape[1],
+                          num_classes = num_classes, fix_length = args.fix_length)
+    elif args.model == 'CNN':
+        model = IMDBCnn(vocab_size=TEXT.vocab.vectors.shape[0], dim_embdeding=TEXT.vocab.vectors.shape[1], num_classes=num_classes)
+    elif args.model == 'LSTM':
+        model = IMDBRnn(vocab_size = TEXT.vocab.vectors.shape[0], dim_embdeding = TEXT.vocab.vectors.shape[1], num_classes=num_classes)
+    
+    if args.pretrained:
+        model.embedding.weight.data = TEXT.vocab.vectors
+        
+    model.embedding.requires_grad = False
+    
+    if use_gpu:
+        model = model.cuda()
+
+
+    
+    optimizer = optim.Adam(model.parameters())
+        
+    
+    
+    train_losses, train_accuracy = [],[]
+    val_losses, val_accuracy = [],[]
+    
+    
+    for epoch in range(args.epochs):
+    
+        epoch_loss, epoch_accuracy = fit(epoch,model,train_iter,phase='training')
+        val_epoch_loss , val_epoch_accuracy = fit(epoch,model,test_iter,phase='validation')
+        train_losses.append(epoch_loss)
+        train_accuracy.append(epoch_accuracy)
+        val_losses.append(val_epoch_loss)
+        val_accuracy.append(val_epoch_accuracy)
+
+
+
+
+
+
---
+++ models.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import torch.nn as nn
+
+
+class Naive_Clf(nn.Module):
+    def __init__(self, vocab_size, dim_embdeding, num_classes=2, fix_length=200):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, dim_embdeding)
+        self.classifier = nn.Linear(dim_embdeding*fix_length, 2)
+        self.classifier = nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(dim_embdeding * fix_length, 2),
+#                nn.ReLU(),
+#                nn.Linear(512, num_classes)
+                )        
+    def forward(self, x):
+#        print(x)
+        out = self.embedding(x).view(x.size(0), -1)
+#        print(embeds.shape)
+        out = self.classifier(out)
+        return out
+    
+class IMDBRnn(nn.Module):
+    def __init__(self, vocab_size, dim_embdeding, num_classes=2, num_layers=1):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, dim_embdeding)
+        h1, h2 =  256, 512
+        self.rnn = nn.LSTM(dim_embdeding, h1, num_layers=num_layers, batch_first=True)
+        self.classifier = nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(h1, num_classes),
+#                nn.Linear(h1, h2),
+#                nn.ReLU(),
+#                nn.Linear(h2, num_classes)
+                )
+        
+#    In the forward function, we pass the input data of size [200, 32], 
+#    which gets passed through the embedding layer and each token in the batch gets replaced by embedding 
+#    and the size turns to [200, 32, 100], where 100 is the embedding dimensions.    
+     
+#    The LSTM layer takes the output of the embedding layer along with two hidden variables. 
+#    The hidden variables should be of the same type of the embeddings output, 
+#    and their size should be [num_layers, batch_size, hidden_size]. 
+
+#    The LSTM processes the data in a sequence and generates the output of the shape [Sequence_length, batch_size, hidden_size], 
+#    where each sequence index represents the output of that sequence. 
+#    In this case, we just take the output of the last sequence, which is of shape [batch_size, hidden_dim]. 
+        
+    def forward(self, x):
+        self.rnn.flatten_parameters()
+
+        e_out = self.embedding(x)
+        rnn_o, _ = self.rnn(e_out) 
+#        print(rnn_o.shape)
+#        rnn_o = rnn_o[-1]
+        rnn_o = rnn_o[:, -1, :].squeeze()
+
+        return self.classifier(rnn_o)
+    
+class IMDBCnn(nn.Module):
+    def __init__(self, vocab_size, dim_embdeding, num_classes, kernel_size=3, fix_length=200):
+        super().__init__()
+
+        self.embedding = nn.Embedding(vocab_size, dim_embdeding)        
+        out_channels, output_size = 256, 10
+        
+        self.cnn = nn.Conv1d(in_channels = fix_length, out_channels=out_channels, kernel_size=3)
+        self.avg = nn.AdaptiveAvgPool1d(output_size = output_size)
+#        self.fc = nn.Linear(256 * 10, num_classes)
+
+        self.classifier = nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(out_channels * output_size, num_classes),
+#                nn.ReLU(),
+#                nn.Linear(512, num_classes)
+                )
+        
+    def forward(self, x):
+
+        e_out = self.embedding(x)
+        
+        cnn_o = self.cnn(e_out) 
+        cnn_avg = self.avg(cnn_o)
+        cnn_avg = cnn_avg.view(x.shape[0], -1)
+        return self.classifier(cnn_avg)
